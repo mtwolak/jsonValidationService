0e96b52d3dfc53c404dd778a7ed973cf115d22a5
Marcin Wolak <mtwolak@gmail.com>
2018-08-04 14:09:03 +0200

    Simple validation controller test

--- src/main/java/com/cs/validation/web/ValidationController.java
+++ src/main/java/com/cs/validation/web/ValidationController.java
@@ -1,5 +1,6 @@
 package com.cs.validation.web;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("validate")
 public class ValidationController {
 
-	@PostMapping
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String validate() {
-		return "Its ok";
+		return "{status: ok}";
 	}
 }
--- src/test/java/com/cs/validation/web/ValidationControllerTest.java
+++ src/test/java/com/cs/validation/web/ValidationControllerTest.java
@@ -6,14 +6,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ValidationApplication.class)
@@ -32,6 +38,20 @@ public class ValidationControllerTest {
 
 	@Test
 	public void shouldValidateSuccessfullyWhenCorrectTradeGiven() throws Exception {
-		mvc.perform(post("/validate")).andExpect(status().isOk());
+		mvc.perform(post("/validate").content(readFile("emp.json")).contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$.status", is("ok")));
+
+	}
+
+	private String readFile(String fileName) {
+		try {
+			ClassLoader classLoader = getClass().getClassLoader();
+			File file = new File(classLoader.getResource(fileName).getFile());
+			return new String(Files.readAllBytes(file.toPath()));
+		} catch (IOException e) {
+			throw new IllegalArgumentException("File not found: " + fileName, e);
+		}
 	}
 }
