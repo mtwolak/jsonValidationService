26e81bec809a4e9ed4114b452278a8eabb01429c
Marcin Wolak <mtwolak@gmail.com>
2018-08-05 16:19:56 +0200

    Swagger info

--- src/main/java/com/cs/SwaggerConf.java
+++ src/main/java/com/cs/SwaggerConf.java
@@ -2,8 +2,11 @@ package com.cs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,20 @@ class SwaggerConf {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage(ValidationApplication.class.getPackage().getName()))
 				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder()
+				.title("Validation REST API")
+				.description("Validation service for trades")
+				.version("1.0")
+				.license("The GNU General Public License v3.0")
+				.licenseUrl("https://www.gnu.org/licenses/gpl-3.0.html")
+				.contact(new Contact("Marcin Wolak", "https://github.com/mtwolak", "mtwolak@gmail.com"))
 				.build();
 	}
+
+
 }
